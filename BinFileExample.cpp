5b1118140abca4f7d91c29cbc001994512d02081
LAPTOP-H7FC3COL\Harris <harris_oik@hotmail.com>
2021-02-16 20:08:02 +0200

    no message

--- BinFileExample.cpp
+++ BinFileExample.cpp
@@ -1,7 +1,5 @@
-// BinFileExample.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
 #include <fstream>
 
 using namespace std;
@@ -29,8 +27,8 @@ int main()
         cout << "Cannot open file!" << endl;
         return 1;
     }
-    Student* StuArray;
-    StuArray = new (nothrow) Student[N];
+    Student* StuArray; 
+	StuArray = new (nothrow) Student[N];
 
     for (int i = 0; i < N; i++)
     {
@@ -65,7 +63,7 @@ int main()
     }
 
     Student* PrintArray;
-    PrintArray = new (nothrow) Student[N];
+	PrintArray = new (nothrow) Student[N];
     for (int i = 0;i < N;i++)
         rf.read((char *) &PrintArray[i], sizeof(Student));
     rf.close();
@@ -92,14 +90,3 @@ int main()
     return 0;
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
